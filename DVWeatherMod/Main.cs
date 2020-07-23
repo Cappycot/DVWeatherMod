fe7471f8ae4e9e254e117c4a8b66ab295575e5a6
Cappycot <chriswang444@gmail.com>
2020-07-22 22:12:05 -0500

    Remove sun disabling feature

--- DVWeatherMod/Main.cs
+++ DVWeatherMod/Main.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityModManagerNet;
 
 namespace DVWeatherMod
@@ -45,14 +46,11 @@ namespace DVWeatherMod
                 return false;
             }
 
-            killedSun = false;
             rainActive = true;
             switchedAudio = false;
 
             return true;
         }
-
-        private static bool killedSun = false;
 
         static void SwapMaterial(GameObject g, Shader s, bool trail)
         {
@@ -69,18 +67,13 @@ namespace DVWeatherMod
         static bool OnToggle(UnityModManager.ModEntry _, bool active)
         {
             rainActive = active;
-            if (killedSun)
-            {
-                Light light2 = GameObject.Find("Directional Light")?.GetComponent<Light>();
-                if (!light2)
-                {
-                    return false;
-                }
-                light2.gameObject.SetActive(true);
-                killedSun = false;
-            }
             if (rain != null)
                 rain.SetActive(active);
+            if (switchedAudio && env_birds != null)
+            {
+                env_birds.clip = active ? rainSFX : defaultSFX;
+                env_birds.Play();
+            }
             return true;
         }
 
@@ -88,36 +81,21 @@ namespace DVWeatherMod
         {
             if (rain == null && rainActive)
             {
-                if (!killedSun)
-                {
-                    // Light light2 = UnityEngine.Object.FindObjectsOfType<Light>().FirstOrDefault((Light light) => light.type == LightType.Directional && light.name == "Directional Light");
-                    Light light2 = GameObject.Find("Directional Light")?.GetComponent<Light>();
-                    if (!light2)
-                    {
-                        return;
-                    }
-                    light2.gameObject.SetActive(false);
-                    killedSun = true;
-                    return; // Make rain in next frame.
-                }
-                else
-                {
-                    rain = GameObject.Instantiate(rainPrefab);
-                    locoCollider = rain.transform.Find("LocoCollider").gameObject;
-                    Transform tf = rain.transform.Find("Rainclouds");
-                    SwapMaterial(tf.gameObject, customSurfaceShader, false);
-                    tf = rain.transform.Find("Rain");
-                    SwapMaterial(tf.gameObject, legacyAlphaBlended, true);
-                    Transform tf2 = tf.Find("Ripple");
-                    SwapMaterial(tf2.gameObject, legacyAlphaBlended, false);
-                    /*tf = tf.transform.Find("Ripple");
-                    SwapMaterial(tf.gameObject, legacyAlphaBlended, false);
-                    Transform tf2 = tf.Find("WaterSpray");
-                    SwapMaterial(tf2.gameObject, legacyAlphaBlended, false);*/
-                    tf2 = tf.Find("ImpactSplash");
-                    SwapMaterial(tf2.gameObject, legacyAlphaBlended, false);
-                    mod.Logger.Log("Successfully switched out materials.");
-                }
+                rain = GameObject.Instantiate(rainPrefab);
+                locoCollider = rain.transform.Find("LocoCollider").gameObject;
+                Transform tf = rain.transform.Find("Rainclouds");
+                SwapMaterial(tf.gameObject, customSurfaceShader, false);
+                tf = rain.transform.Find("Rain");
+                SwapMaterial(tf.gameObject, legacyAlphaBlended, true);
+                Transform tf2 = tf.Find("Ripple");
+                SwapMaterial(tf2.gameObject, legacyAlphaBlended, false);
+                /*tf = tf.transform.Find("Ripple");
+                SwapMaterial(tf.gameObject, legacyAlphaBlended, false);
+                Transform tf2 = tf.Find("WaterSpray");
+                SwapMaterial(tf2.gameObject, legacyAlphaBlended, false);*/
+                tf2 = tf.Find("ImpactSplash");
+                SwapMaterial(tf2.gameObject, legacyAlphaBlended, false);
+                mod.Logger.Log("Successfully switched out materials.");
             }
             else if (!switchedAudio && rainActive)
             {
@@ -127,15 +105,17 @@ namespace DVWeatherMod
                 if (defaultSFX == null)
                     defaultSFX = env_birds.clip;
                 env_birds.clip = rainSFX;
-                env_birds.volume = 1f;
+                // env_birds.volume = 1f;
                 env_birds.Play();
                 switchedAudio = true;
             }
 
-            if (PlayerManager.PlayerTransform == null || !rainActive)
+            Transform playerTransform = PlayerManager.PlayerTransform;
+            if (playerTransform == null || !rainActive)
                 return;
 
-            rain.transform.position = PlayerManager.PlayerTransform.position;
+            // TODO: Figure out how world height works.
+            rain.transform.position = new Vector3(playerTransform.position.x, Mathf.Min(playerTransform.position.y + 100f, 250f), playerTransform.position.z);
             // Hope this isn't too costly.
             // TODO: Set up hitboxes for each loco interior.
             locoCollider.SetActive(PlayerManager.Car != null && PlayerManager.Car.IsLoco);
